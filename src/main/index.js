e516d9f1242d800028765cb82a3e34955bad0173
N S LAKSHMI SAGAR <147676339+LakshmiSagarNS@users.noreply.github.com>
2024-04-10 18:26:06 +0530

    Update index.js

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const exeRoutes = require('../route/routes');
+const datasetsRoutes = require('../route/routes');
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get("/",(req,res)=>{res.send("Hello World");});
-app.use("/api/v1/exe",exeRoutes);
+//app.get("/",(req,res)=>{res.send("Hello World");});
+app.use("/api/v1/datasets",exeRoutes);
 
 app.listen(port,() => console.log(`app listening on port ${port}`));
