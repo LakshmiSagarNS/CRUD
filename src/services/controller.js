c9ad3f6d43cad594375251c9617ebec7e6f5fdb6
N S LAKSHMI SAGAR <147676339+LakshmiSagarNS@users.noreply.github.com>
2024-04-10 19:25:52 +0530

    Update controller.js

--- src/services/controller.js
+++ src/services/controller.js
@@ -1,20 +1,20 @@
-const pool =require('../config/db.js');
+const pool = require('../config/db.js');
 const queries = require('./queries');
-const getdata=(req, res)=> {
+const getData = (req, res)=> {
     pool.query(queries.getdata, (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
     });
 }
-const getDataById =(req,res)=>{
+const getDataById = (req,res)=>{
     const id=parseInt(req.params.id);
     pool.query(queries.getDataById,[id],(error,results)=>{
         if(error) throw error;
         res.status(200).json(results.rows);
     });
 };
-const addData=(req,res)=>{
-    const data=req.body;
+const addData = (req,res)=>{
+    const data = req.body;
     // check if id exist
     pool.query(queries.checkIdExists,parseInt([data.id]),(error,results)=>{
         if(results.rows.length){
@@ -29,9 +29,9 @@ const addData=(req,res)=>{
 };
 
 const removeData = (req,res)=>{
-    const id= parseInt(req.params.id);
+    const id = parseInt(req.params.id);
     pool.query(queries.checkIdExists,[id],(error,results)=>{
-        const noDataFound =!results.rows.length;
+        const noDataFound = !results.rows.length;
         if(noDataFound){
         res.status(204).send("data does not exist in the database");
         }
@@ -43,14 +43,14 @@ const removeData = (req,res)=>{
     });
 };
 const updateData = (req,res)=>{
-    const id= parseInt(req.params.id);
+    const id = parseInt(req.params.id);
     pool.query(queries.checkIdExists,[id],(error,results)=>{
-        const noDataFound =!results.rows.length;
+        const noDataFound = !results.rows.length;
         if(noDataFound){
         res.status(200).send("data does not exist in the database");
         }
     
-        const update_data=req.body;
+        const update_data = req.body;
     pool.query(queries.updateData,[ update_data.dataset_id,update_data.type,update_data.name,update_data.validation_config,update_data.extraction_config,update_data.dedup_config,update_data.data_schema,update_data.denorm_config,update_data.router_config,update_data.dataset_config,update_data.tags,update_data.data_version,update_data.status,update_data.created_by,update_data.updated_by,update_data.created_date,update_data.updated_date,update_data.published_date,id],(error,results)=>{
         if(error) throw error;
         res.status(200).send("data updated sucessfully");
@@ -61,7 +61,7 @@ const updateData = (req,res)=>{
 
 
 module.exports = {
-    getdata,
+    getData,
     getDataById,
     addData,
     removeData,
